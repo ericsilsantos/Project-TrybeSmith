a21aaac9e571af30daae11c79ec031221f5c3e30
Eric Santos <eric.silvasantos44@gmail.com>
2023-01-24 12:22:00 -0300

    modificação

--- src/controllers/ordersController.ts
+++ src/controllers/ordersController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import ordersService from '../services/ordersService';
 
 const ordersController = {
-  async getAll(req: Request, res: Response) {
+  async getAll(_req: Request, res: Response) {
     const result = await ordersService.getAll();
     res.status(200).json(result);
   },
